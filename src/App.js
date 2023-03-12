d67c49e19870ea3b43540068b6a748fd40554fa9
Sushil <shil7276@gmail.com>
2023-03-13 00:24:36 +0530

    Navbar and MovieCard add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import MovieCard from './components/MovieCard';
+import NavBar from './components/NavBar';
+import { data } from "./data"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <div className='main'>
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourites</div>
+        </div>
+
+        <div className="list">
+          {data.map(movie => (
+            <MovieCard movie={movie} />
+          ))}
+
+        </div>
+      </div>
     </div>
   );
 }

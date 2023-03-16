171a51f1bda91db18c18d5678e1dffe51c0139d0
Sushil <shil7276@gmail.com>
2023-03-16 13:59:28 +0530

    Add Store and Subcribe to store

--- src/App.js
+++ src/App.js
@@ -3,25 +3,46 @@ import MovieCard from './components/MovieCard';
 import NavBar from './components/NavBar';
 import { data } from "./data"
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <div className='main'>
-        <div className="tabs">
-          <div className="tab">Movies</div>
-          <div className="tab">Favourites</div>
-        </div>
+import React, { Component } from 'react'
 
-        <div className="list">
-          {data.map(movie => (
-            <MovieCard movie={movie} />
-          ))}
+export default class App extends Component {
+  componentDidMount() {
 
+    const { store } = this.props;
+    store.subscribe(() => {
+      console.log("Update")
+      this.forceUpdate();
+    })
+
+    store.dispatch({
+      type: "ADD_MOVIES",
+      movies: data
+    });
+
+    console.log("State", store.getState());
+  }
+  render() {
+
+    const movies = this.props.store.getState();
+console.log("Render")
+    return (
+      <div className="App">
+        <NavBar />
+        <div className='main'>
+          <div className="tabs">
+            <div className="tab">Movies</div>
+            <div className="tab">Favourites</div>
+          </div>
+
+          <div className="list">
+            {movies.map((movie, index) => (
+              <MovieCard movie={movie} key={`movies-${index}`} />
+            ))}
+
+          </div>
         </div>
       </div>
-    </div>
-  );
+    )
+  }
 }
 
-export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { legacy_createStore } from 'redux';
+import movies from "./reducers/index"
+
+const store = legacy_createStore(movies)
+
+// store.dispatch({
+//   type:"ADD_MOVIES",
+//   movies:[{name:"SuperMan"}]
+// })
+// console.log("After State", store.getState())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App store={store} />
   </React.StrictMode>
 );
